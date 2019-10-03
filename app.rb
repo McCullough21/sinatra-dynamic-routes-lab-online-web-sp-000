20842d4a803d7ef6c127dc0842361f33e71ef0eb
Michael McCullough <michael.mccullough21@gmail.com>
2019-10-03 16:00:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @number = params[:number]
     @phrase = params[:phrase]
-    @response = @number.to_i.times{@phrase}.join
+    @response = @number.to_i.times {"#{@phrase}"}.join
     "#{@response}"
   end
 
