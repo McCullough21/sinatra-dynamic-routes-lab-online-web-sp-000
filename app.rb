6bfea85d1edefaeb5386978bc8fc6c12c61476eb
Michael McCullough <michael.mccullough21@gmail.com>
2019-10-03 15:44:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
 
+  get "/reversename/:name" do
+    @name = params[:name]
+    "#{@name}.reverse"
+  end
+
+  get "/square/:number"
+  @num = params[:number].to_i % params[:number].to_i
+  "#{@num}"
+  end
+
+ 
 end
