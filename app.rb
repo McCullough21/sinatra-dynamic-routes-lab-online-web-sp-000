4a3219db66f59d5ba586f44b7bbab3df28c6add6
Michael McCullough <michael.mccullough21@gmail.com>
2019-10-03 16:05:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,18 +7,18 @@ class App < Sinatra::Base
     "#{@name}"
   end
 
-  get "/square/:number" do
-  @num = params[:number].to_i * params[:number].to_i
-  "#{@num}"
-  end
-
-  get "/say/:number/:phrase" do
-    @number = params[:number].to_i
-    @phrase = params[:phrase]
-    @response = @number.times {"#{@phrase}"}
-    "#{@response.}".join
-    binding.pry
-  end
+  # get "/square/:number" do
+  # @num = params[:number].to_i * params[:number].to_i
+  # "#{@num}"
+  # end
+  #
+  # get "/say/:number/:phrase" do
+  #   @number = params[:number].to_i
+  #   @phrase = params[:phrase]
+  #   @response = @number.times {"#{@phrase}"}
+  #   "#{@response.}".join
+  #   binding.pry
+  # end
 
 
 end
