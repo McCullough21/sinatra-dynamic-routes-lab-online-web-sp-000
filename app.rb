d6406c5361d3bfb7d9ee9a40edfc2116272e39b6
Michael McCullough <michael.mccullough21@gmail.com>
2019-10-03 16:37:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,12 +22,20 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
-  get "/:operation/:number1/:number2" do
-    @number1 = params[:number1].to_i
-    @number2 = params[:number2].to_i
-    @operation = params[:operation]
-
-    @number1.send(@operation,@number2)
+  get '/:operation/:number1/:number2' do
+    @oper = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    case @oper
+      when "subtract"
+        (@num1 - @num2).to_s
+      when "add"
+        (@num1 + @num2).to_s
+      when "multiply"
+        (@num1 * @num2).to_s
+      when "divide"
+        (@num1 / @num2).to_s
+    end
   end
 
 end
