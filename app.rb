ca42f4c89f7d35d2afe06d86d083e81e7fa99ccc
Michael McCullough <michael.mccullough21@gmail.com>
2019-10-03 15:48:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,5 +12,13 @@ class App < Sinatra::Base
   "#{@num}"
   end
 
+  get "/say/:number/:phrase" do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    @number.times do
+    "#{@phrase}"
+  end
+  end
+
 
 end
