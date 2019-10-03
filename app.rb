a3efd28994da7236689b472cad3408b34c718a4b
Michael McCullough <michael.mccullough21@gmail.com>
2019-10-03 16:33:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,7 +26,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
      @num1 = params[:number1]
      @num2 = params[:number2]
-     @answer = @num1.send(@operation,@num2)
+      @num1.send(@operation,@num2)
   end
 
 end
